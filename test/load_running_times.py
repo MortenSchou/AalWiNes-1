4c65216e6b173488952da237c3716102aa792200
Dan Kristiansen <dankris07@gmail.com>
2020-04-24 14:36:54 +0200

    Update

--- test/load_running_times.py
+++ test/load_running_times.py
@@ -23,17 +23,28 @@ red0_win = 0
 red1_win = 0
 red2_win = 0
 red3_win = 0
+engine_ratio = []
+
+#Moped vs post
+moped_post = [[0 for s in range(4)] for i in range(4)]
+#Moped vs pre
+moped_pre = [[0 for s in range(4)] for i in range(4)]
+post_pre = [[0 for s in range(4)] for i in range(4)]
 
 bin_hash_split = bin_hash.split("-")
 
 file = open('test_results.csv', 'w')
+file_all = open('test_results_all.csv', 'w')
 with file:
     writer = csv.writer(file)
-    fnames = ['Network', 'Nodes', 'Labels', 'Rules', 'PDA-States', "PDA-Rules", "States_reduction", "Rules_reduction",
+    writer_all = csv.writer(file_all)
+    fnames = ['Network', 'Size-factor', 'Nodes', 'Labels', 'Rules', 'PDA-States', "PDA-Rules", "States_reduction", "Rules_reduction",
               'Query-Fails', 'Query-Type',
               'Engine', 'Reduction', 'Compilation-time', 'Reduction-time', 'Verification-time', 'Total-time']
     writer = csv.DictWriter(file, fieldnames=fnames)
+    writer_all = csv.DictWriter(file_all, fieldnames=fnames)
     writer.writeheader()
+    writer_all.writeheader()
 
     tests =[]
     for t in ['Aarnet', 'Agis', 'Bandcon', 'Bellcanada', 'Claranet', 'DeutscheTelekom']:
@@ -95,23 +106,14 @@ with file:
             if not files:
                 continue
             queries = []
-            newFiles = []
-            # Modify file to work
-            for fi in files:
-                line_nr = 0
-                writeobject = ""
-                for line in open(fi):
-                    if line_nr == 7:
-                        line = line[1:]
-                    writeobject = writeobject + line
-                    line_nr = line_nr + 1
-                newFiles.append(writeobject)
 
             for i in range(len(files)):
-                #jd = json.load(open(files[i]))
-                jd = json.loads(newFiles[i])
+                jd = json.load(open(files[i]))
                 name = files[i].parts[1]
-                queries.append({"Name": name, "Data": jd["answers"]["Q1"]})
+                test_type = [s for s in name.split('-') if s[1:].isdigit()]
+                engine = test_type[0]
+                reduction = test_type[1]
+                queries.append({"Name": name, "Reduction": reduction, "Engine": engine, "Data": jd["answers"]["Q1"]})
 
             # Get universal network stats
             test_numbers = [int(s[1:])
@@ -121,17 +123,26 @@ with file:
             Q_fails = test_numbers[1]
 
             for q in queries:
-                test_type = [s[1:] for s in q['Name'].split('-') if s[1:].isdigit()]
-                engine = test_type[0]
-                reduction = test_type[1]
+                engine = q['Engine']
+                reduction = q['Reduction']
                 q = q['Data']
-                if engine == 2:
-                    reductions.append(
-                        {'Network': test_name[0], "R": reduction, "Vtime": q['verification-time'] + q['reduction-time']})
                 verification_compilation.append(
                     {"Engine": engine, "Reduction": reduction, "Total": q['verification-time'] + q['compilation-time'] + q['reduction-time'],
                         "Compilation-time": q["compilation-time"], "Verification-time": q['verification-time'], "Reduction-time": q["reduction-time"]})
-                writer.writerow({'Network': test_name[0], 'Nodes': q['network_node_size'], 'Labels': q['network_label_size'],
+                
+                if len([s for s in queries if s['Engine'] == engine]) == 4:
+                    writer.writerow({'Network': test_name[0], 'Size-factor': test_name[1], 'Nodes': q['network_node_size'], 'Labels': q['network_label_size'],
+                                'Rules': q['network_rules_size'], 'PDA-States': q['pda_states_rules'][0], 'PDA-Rules': q['pda_states_rules'][1],
+                                'States_reduction': q['pda_states_rules_reduction'][0], 'Rules_reduction': q['pda_states_rules_reduction'][1],
+                                'Query-Fails': Q_fails, 'Query-Type': Q_type,
+                                'Engine': engine, 'Reduction': reduction, 'Compilation-time': q['compilation-time'], 'Reduction-time': q['reduction-time'],
+                                'Verification-time': q['verification-time'], 'Total-time': q['verification-time'] + q['compilation-time'] + q['reduction-time']})
+                    if engine == "E2":
+                        reductions.append({ 'Network': test_name[0], 'PDA-States': q['pda_states_rules'][0], 'PDA-Rules': q['pda_states_rules'][1],
+                                            'States_reduction': q['pda_states_rules_reduction'][0], 'Rules_reduction': q['pda_states_rules_reduction'][1], 
+                                            "R": reduction, "Vtime": q['verification-time'] + q['reduction-time']})
+
+                writer_all.writerow({'Network': test_name[0], 'Size-factor': test_name[1], 'Nodes': q['network_node_size'], 'Labels': q['network_label_size'],
                                 'Rules': q['network_rules_size'], 'PDA-States': q['pda_states_rules'][0], 'PDA-Rules': q['pda_states_rules'][1],
                                 'States_reduction': q['pda_states_rules_reduction'][0], 'Rules_reduction': q['pda_states_rules_reduction'][1],
                                 'Query-Fails': Q_fails, 'Query-Type': Q_type,
@@ -139,24 +150,51 @@ with file:
                                 'Verification-time': q['verification-time'], 'Total-time': q['verification-time'] + q['compilation-time'] + q['reduction-time']})
 
             sorted_verification_data = sorted(
-                verification_compilation, key=lambda k: k['Total'])
+                verification_compilation, key=lambda k: k['Verification-time'])
 
+            Percentage = 0.5
+            Second = 0.001
             # Find fastest engine for verification + reduction
-            if sorted_verification_data[0]['Engine'] == '3':
-                pre_win = pre_win + 1
-            elif sorted_verification_data[0]['Engine'] == '2':
-                post_win = post_win + 1
-            elif sorted_verification_data[0]['Engine'] == '1':
-                moped_win = moped_win + 1
 
-            if sorted_verification_data[0]['Reduction'] == '0':
-                red0_win = red0_win + 1
-            elif sorted_verification_data[0]['Reduction'] == '1':
-                red1_win = red1_win + 1
-            elif sorted_verification_data[0]['Reduction'] == '2':
-                red2_win = red2_win + 1
-            elif sorted_verification_data[0]['Reduction'] == '3':
-                red3_win = red3_win + 1
+
+            i = -1
+            for r in ["R0", "R1", "R2", "R3"]:
+                i = i + 1
+                list_post_moped = [s for s in sorted_verification_data if s['Reduction'] == r and s['Engine'] != 'E3']
+                if len(list_post_moped) == 2:
+                    #Even
+                    if (list_post_moped[1]['Verification-time'] - list_post_moped[0]['Verification-time']) / list_post_moped[1]['Verification-time'] < Percentage or list_post_moped[1]['Verification-time'] - list_post_moped[0]['Verification-time'] < Second:
+                        moped_post[2][i] = moped_post[2][i] + 1
+                    elif list_post_moped[0]['Engine'] == 'E1':
+                        moped_post[0][i] = moped_post[0][i] + 1
+                    elif list_post_moped[0]['Engine'] == 'E2':
+                        moped_post[1][i] = moped_post[1][i] + 1
+                else:
+                    moped_post[3][i] = moped_post[3][i] + 1
+
+                list_pre_moped = [s for s in sorted_verification_data if s['Reduction'] == r and s['Engine'] != 'E2']
+                if len(list_pre_moped) == 2: 
+                    #Even
+                    if (list_pre_moped[1]['Verification-time'] - list_pre_moped[0]['Verification-time']) / list_pre_moped[1]['Verification-time'] < Percentage or list_pre_moped[1]['Verification-time'] - list_pre_moped[0]['Verification-time'] < Second:
+                        moped_pre[2][i] = moped_pre[2][i] + 1
+                    elif list_pre_moped[0]['Engine'] == 'E1':
+                        moped_pre[0][i] = moped_pre[0][i] + 1
+                    elif list_pre_moped[0]['Engine'] == 'E3':
+                        moped_pre[1][i] = moped_pre[1][i] + 1
+                else:
+                    moped_pre[3][i] = moped_pre[3][i] + 1
+
+                list_post_pre = [s for s in sorted_verification_data if s['Reduction'] == r and s['Engine'] != 'E1']
+                if len(list_post_pre) == 2: 
+                    #Even
+                    if (list_post_pre[1]['Verification-time'] - list_post_pre[0]['Verification-time']) / list_post_pre[1]['Verification-time'] < Percentage or list_post_pre[1]['Verification-time'] - list_post_pre[0]['Verification-time'] < Second:
+                        post_pre[2][i] = post_pre[2][i] + 1
+                    elif list_post_pre[0]['Engine'] == 'E2':
+                        post_pre[0][i] = post_pre[0][i] + 1
+                    elif list_post_pre[0]['Engine'] == 'E3':
+                        post_pre[1][i] = post_pre[1][i] + 1
+                else:
+                    post_pre[3][i] = post_pre[3][i] + 1
 
             test_data.append({"Network": {"Nodes": queries[0]['Data']["network_node_size"], "Labels": queries[0]['Data']["network_label_size"], "Rules": queries[0]['Data']["network_rules_size"],
                                         "States": queries[0]['Data']["pda_states_rules"][0], "PDA_Rules": queries[0]['Data']["pda_states_rules"][1],
@@ -168,57 +206,151 @@ with file:
             print(f)
             print(e)
 
-for t in test_data:
+
+sorted_test_data = sorted(test_data, key=lambda k: k['Verification'][0]['Total'])
+
+for t in sorted_test_data:
     print(t)
 
-print("Moped win:" + str(moped_win))
-print("Post* win:" + str(post_win))
-print("Pre* win:" + str(pre_win))
+
+#Print fastest overall time
+print("MopedPost")
+print(moped_post)
+
+i = -1
+for E in moped_post:
+    i = i + 1
+    if i == 0:
+        engine = "Moped"
+    elif i == 1:
+        engine = "Post"
+    elif i == 2:
+        engine = "Even"
+    elif i == 3:
+        engine = "Unknown"
+    print(engine + "&" + str(E[0]) + "&" + str(E[1]) + "&" + str(E[2]) + "&" + str(E[3]) + "\\\ \hline")
+
+print("moped_pre")
+print(moped_pre)
+i = -1
+for E in moped_pre:
+    i = i + 1
+    if i == 0:
+        engine = "Moped"
+    elif i == 1:
+        engine = "Pre"
+    elif i == 2:
+        engine = "Even"
+    elif i == 3:
+        engine = "Unknown"
+    print(engine + "&" + str(E[0]) + "&" + str(E[1]) + "&" + str(E[2]) + "&" + str(E[3]) + "\\\ \hline")
+
+print("post_pre")
+print(post_pre)
+i = -1
+for E in post_pre:
+    i = i + 1
+    if i == 0:
+        engine = "Post"
+    elif i == 1:
+        engine = "Pre"
+    elif i == 2:
+        engine = "Even"
+    elif i == 3:
+        engine = "Unknown"
+    print(engine + "&" + str(E[0]) + "&" + str(E[1]) + "&" + str(E[2]) + "&" + str(E[3]) + "\\\ \hline")
 
 print("Reduction0 win:" + str(red0_win))
 print("Reduction1 win:" + str(red1_win))
 print("Reduction2 win:" + str(red2_win))
 print("Reduction3 win:" + str(red3_win))
 
-#buffer = {"Network": reductions[0]['Network'], "R0": 0, "R1": 0, "R2": 0, "R3": 0}
-#bufferNetworks = []
-
-# Reductions
-# for ri in range(len(reductions[3::4])):
-#    network = str(reductions[ri]['Network'])
-#    if network != buffer['Network']:
-#        bufferNetworks.append({"Network": buffer['Network'], "R0": buffer['R0'], "R1": buffer['R1'], "R2": buffer['R2'], "R3": buffer['R3']})
-#        nets = [net for net in bufferNetworks if net['Network'] == network]
-#        if len(nets) == 1:
-#            buffer = bufferNetworks.pop(bufferNetworks.index(nets[0]))
-#        else:
-#            buffer = {"Network": network, "R0": reductions[ri-3]['Vtime'], "R1": reductions[ri-2]['Vtime'], "R2": reductions[ri-1]['Vtime'], "R3": reductions[ri]['Vtime']}
-#    buffer['R0'] = (buffer['R0'] + reductions[ri-3]['Vtime']) / 2
-#    buffer['R1'] = (buffer['R1'] + reductions[ri-2]['Vtime']) / 2
-#    buffer['R2'] = (buffer['R2'] + reductions[ri-1]['Vtime']) / 2
-#    buffer['R3'] = (buffer['R3'] + reductions[ri]['Vtime']) / 2
-
-
-# print("\nLatex Table Rows")
-#bufferNetworks.append({"Network": buffer['Network'], "R0": buffer['R0'], "R1": buffer['R1'], "R2": buffer['R2'], "R3": buffer['R3']})
-# for buffer in bufferNetworks:
-#    speedup = ((buffer['R0'] - buffer['R3'])/buffer['R3'])
-#    print(buffer['Network'] + "&" + str(round(buffer['R0'],3)) + "&" + str(round(buffer['R1'],3)) + "&" + str(round(buffer['R2'],3)) + "&" + str(round(buffer['R3'],3))+ "&" + str(round(speedup,3)) + "\\\ \hline")
-
-for t in test_data[:10]:
+for t in sorted_test_data[:10]:
     print(str(t['Network']['Nodes']) + "&" + str(t['Network']['Rules']) + "&" + str(t['Network']['Labels']) + "&" +
           str(t['Query']['Type']) + ";" + str(t['Query']['Failover']) + "&" +
-          str(t['Verification'][0]['Engine']) + "&" + str(t['Verification'][0]['Reduction']) + "&" + str(round(t['Verification'][0]['Verification-time'], 3)) + "&" + str(round(t['Verification'][0]['Total'], 3)) +
+          str(t['Verification'][0]['Engine']) + "&" + str(t['Verification'][0]['Reduction']) + "&" + str(round(t['Verification'][0]['Verification-time'], 5)) + "&" + str(round(t['Verification'][0]['Total'], 3)) +
           "\\\ \hline")
 
 print("last 10")
 
-for t in test_data[len(test_data)-10:]:
-    print(str(t['Network']['Nodes']) + "&" + str(t['Network']['Rules']) + ";" + str(t['Network']['Labels']) + "&" +
+for t in sorted_test_data[len(test_data)-10:]:
+    print(str(t['Network']['Nodes']) + "&" + str(t['Network']['Rules']) + "&" + str(t['Network']['Labels']) + "&" +
           str(t['Query']['Type']) + ";" + str(t['Query']['Failover']) + "&" +
-          str(t['Verification'][0]['Engine']) + "&" + str(t['Verification'][0]['Reduction']) + "&" + str(round(t['Verification'][0]['Verification-time'], 3)) + "&" + str(round(t['Verification'][0]['Total'], 3)) +
+          str(t['Verification'][0]['Engine']) + "&" + str(t['Verification'][0]['Reduction']) + "&" + str(round(t['Verification'][0]['Verification-time'], 5)) + "&" + str(round(t['Verification'][0]['Total'], 3)) +
           "\\\ \hline")
 
-# print("\nLatex Table Rows ALL")
-# for t in sorted_data:
-# print(t['Name'].replace('_', '\_')[6:] + "&" + str(round(t['Compilation-time'], 3)) + "&" + str(round(t['Compilation ration'], 3)) + "&" + str(round(t['Vtime Moped'], 3)) + "&" + str(round(t['Vtime Post*'], 3)) + "&" + str(round(t['Ratio(E1/E2)'], 3)) + "\\\ \hline")
+
+#Latex print fastest reduction on fastest engine = 2 :)
+bufferNetworks = []
+buffer = {"Network": reductions[0]['Network'], "R0": 0, "R1": 0, "R2": 0, "R3": 0}
+try:
+    print("Fastest reduction")
+    # Reductions
+    for r in reductions:
+        network = str(r['Network'])
+        reduction = str(r['R'])
+        if network != buffer['Network']:
+            bufferNetworks.append({"Network": buffer['Network'], "R0": buffer['R0'], "R1": buffer['R1'], "R2": buffer['R2'], "R3": buffer['R3']})
+            nets = [net for net in bufferNetworks if net['Network'] == network]
+            if len(nets) == 1:
+                buffer = bufferNetworks.pop(bufferNetworks.index(nets[0]))
+            else:
+                buffer = {"Network": network, "R0": 0, "R1": 0, "R2": 0, "R3": 0}
+        buffer[reduction] = (buffer[reduction] + r['Vtime']) / 2
+
+except BaseException as e:
+    print(e)
+
+# print("\nLatex Table Rows")
+bufferNetworks.append({"Network": buffer['Network'], "R0": buffer['R0'], "R1": buffer['R1'], "R2": buffer['R2'], "R3": buffer['R3']})
+for buffer in bufferNetworks:
+    if buffer['R3'] != 0:
+        speedup = ((buffer['R0'] - buffer['R3'])/buffer['R3'])
+        print(buffer['Network'] + "&" + str(round(buffer['R0'],3)) + "&" + str(round(buffer['R1'],3)) + "&" + str(round(buffer['R2'],3)) + "&" + str(round(buffer['R3'],3))+ "&" + str(round(speedup,3)) + "\\\ \hline")
+
+#Latex print fastest engine on fastest reducktion = 0
+
+engine_ratio_moped_post = []
+engine_ratio_moped_pre = []
+engine_ratio_post_pre = []
+
+def get_ratio(engineA, engineB):
+    engine_ratio = []
+    for t in test_data:
+        i = -1
+        for r in ["R0", "R1", "R2", "R3"]:
+            try:
+                first = next(s for s in t['Verification'] if s['Engine'] == engineA and s['Reduction'] == r)
+                fastest_time = first['Verification-time']
+                next_veri = next(s for s in t['Verification'] if s['Engine'] == engineB and s['Reduction'] == r)
+                if  abs(next_veri['Verification-time'] - fastest_time) < 0.001: continue
+            except:
+                continue
+            try:
+                ratio =  fastest_time / next_veri['Verification-time']
+                engine_ratio.append({"Test": t, "FT": fastest_time, "R": r, "NT": next_veri['Verification-time'], "NE": next_veri['Engine'], "Ratio": ratio})
+            except:
+                print("some")
+    sorted_engine_ratio = sorted(engine_ratio, key=lambda k: k['Ratio'])
+    print("Fastest engine" + engineA + engineB)
+    print("\\begin{table}[H]\n\\centering\n\\begin{tabular}{|c|c|c|c|c|c|c|c|c|c|}\n \hline \n \\rowcolor[HTML]{9B9B9B}\n \\textbf{Nodes}& \\textbf{Rules} & \\textbf{Labels} & \\textbf{PDA-S} & \\textbf{PDA-R} & \\textbf{Query} & \\textbf{R} & \\textbf{V1} & \\textbf{V2} & \\textbf{Ratio}\\\ \hline")
+    for t in sorted_engine_ratio[:5]:
+        print(str(t['Test']['Network']['Nodes']) + "&" +  str(t['Test']['Network']['Rules']) + "&" + str(t['Test']['Network']['Labels']) 
+                + "&" + str(t['Test']['Network']['States_reduction']) + "&" + str(t['Test']['Network']['Rules_reduction']) + "&" + str(t['Test']['Query']) + "&" + str(t['R'])
+                + "&" + str(round(t['FT'], 4)) + "&" + str(round(t['NT'], 4)) + "&" + str(round(t['Ratio'], 4)) + "\\\ \hline")
+
+    print("$\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$\\\ \hline")
+    mid = int(len(sorted_engine_ratio)/2)
+    for t in sorted_engine_ratio[mid : mid+5]:
+        print(str(t['Test']['Network']['Nodes']) + "&" +  str(t['Test']['Network']['Rules']) + "&" + str(t['Test']['Network']['Labels']) 
+                + "&" + str(t['Test']['Network']['States_reduction']) + "&" + str(t['Test']['Network']['Rules_reduction']) + "&" + str(t['Test']['Query']) + "&" + str(t['R'])
+                + "&" + str(round(t['FT'], 4)) + "&" + str(round(t['NT'], 4)) + "&" + str(round(t['Ratio'], 4)) + "\\\ \hline")
+    print("$\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$\\\ \hline")
+    for t in sorted_engine_ratio[len(sorted_engine_ratio)-5:]:
+        print(str(t['Test']['Network']['Nodes']) + "&" +  str(t['Test']['Network']['Rules']) + "&" + str(t['Test']['Network']['Labels']) 
+                + "&" + str(t['Test']['Network']['States_reduction']) + "&" + str(t['Test']['Network']['Rules_reduction']) + "&" + str(t['Test']['Query']) + "&" + str(t['R'])
+                + "&" + str(round(t['FT'], 4)) + "&" + str(round(t['NT'], 4)) + "&" + str(round(t['Ratio'], 4)) + "\\\ \hline")
+
+get_ratio("E1","E2")
+get_ratio("E1","E3")
+get_ratio("E2","E3")
