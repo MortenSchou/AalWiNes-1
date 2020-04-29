49a772d067b685a9afc2c628aa4c5705a7a71b69
Dan Kristiansen <dankris07@gmail.com>
2020-04-29 11:12:45 +0200

    Update with MI

--- test/load_running_times.py
+++ test/load_running_times.py
@@ -31,7 +31,38 @@ moped_post = [[0 for s in range(4)] for i in range(4)]
 moped_pre = [[0 for s in range(4)] for i in range(4)]
 post_pre = [[0 for s in range(4)] for i in range(4)]
 
+mipost_post = [[0 for s in range(4)] for i in range(4)]
+
 bin_hash_split = bin_hash.split("-")
+
+def find_win_MI(enginea, engineb, i, reduction, con_win, Percentage, Second, sorted_verification_data):
+    list_win = [s for s in sorted_verification_data if s['Reduction'] == reduction and (s['Engine'] == enginea or s['Engine'] == engineb)]
+    if len(list_win) == 2:
+        #Even
+        if (list_win[1]['Verification-time'] - list_win[0]['Verification-time']) / list_win[1]['Verification-time'] < Percentage or list_win[1]['Verification-time'] - list_win[0]['Verification-time'] < Second:
+            con_win[2][i] = con_win[2][i] + 1
+        elif list_win[0]['MI'] == 1:
+            con_win[0][i] = con_win[0][i] + 1
+        elif list_win[0]['MI'] == 0:
+            con_win[1][i] = con_win[1][i] + 1
+    else:
+        con_win[3][i] = con_win[3][i] + 1
+    return con_win
+
+def find_win(enginea, engineb, i, reduction, con_win, Percentage, Second, sorted_verification_data):
+    list_win = [s for s in sorted_verification_data if s['Reduction'] == reduction and (s['Engine'] == enginea or s['Engine'] == engineb) and s['MI'] == 0]
+    if len(list_win) == 2:
+        #Even
+        if (list_win[1]['Verification-time'] - list_win[0]['Verification-time']) / list_win[1]['Verification-time'] < Percentage or list_win[1]['Verification-time'] - list_win[0]['Verification-time'] < Second:
+            con_win[2][i] = con_win[2][i] + 1
+        elif list_win[0]['Engine'] == enginea:
+            con_win[0][i] = con_win[0][i] + 1
+        elif list_win[0]['Engine'] == engineb:
+            con_win[1][i] = con_win[1][i] + 1
+    else:
+        con_win[3][i] = con_win[3][i] + 1
+    return con_win
+
 
 file = open('test_results.csv', 'w')
 file_all = open('test_results_all.csv', 'w')
@@ -54,54 +85,127 @@ with file:
                     tests.append(t + "-" + str(N) + "-Q" + str(Q) + "-k" + str(k))
     for f in tests:
         files = []
+        MI_Files = []
         verification_compilation = []
+        mi_post_veri = []
         myfile = Path("results/" + bin_hash + "/" + f)
         if myfile.is_file():
             files.append(myfile)
+
+        myfile = Path(
+            "results/" + bin_hash_split[0] + "_MI-" + bin_hash_split[1] + "-E1-R0/" + f)
+        if myfile.is_file():
+            MI_Files.append(myfile)
+        
         myfile = Path(
             "results/" + bin_hash_split[0] + "-" + bin_hash_split[1] + "-E1-R1/" + f)
         if myfile.is_file():
             files.append(myfile)
+        
+        myfile = Path(
+            "results/" + bin_hash_split[0] + "_MI-" + bin_hash_split[1] + "-E1-R1/" + f)
+        if myfile.is_file():
+            MI_Files.append(myfile)
+
         myfile = Path(
             "results/" + bin_hash_split[0] + "-" + bin_hash_split[1] + "-E1-R2/" + f)
         if myfile.is_file():
             files.append(myfile)
+
+        myfile = Path(
+            "results/" + bin_hash_split[0] + "_MI-" + bin_hash_split[1] + "-E1-R2/" + f)
+        if myfile.is_file():
+            MI_Files.append(myfile)
+
         myfile = Path(
             "results/" + bin_hash_split[0] + "-" + bin_hash_split[1] + "-E1-R3/" + f)
         if myfile.is_file():
             files.append(myfile)
+
+        myfile = Path(
+            "results/" + bin_hash_split[0] + "_MI-" + bin_hash_split[1] + "-E1-R3/" + f)
+        if myfile.is_file():
+            MI_Files.append(myfile)
+        
         myfile = Path("results/" + bin_hash_split[0] + "-" +
                       bin_hash_split[1] + "-E2-" + bin_hash_split[3] + "/" + f)
         if myfile.is_file():
             files.append(myfile)
+
+        myfile = Path("results/" + bin_hash_split[0] + "_MI-" +
+                      bin_hash_split[1] + "-E2-" + bin_hash_split[3] + "/" + f)
+        if myfile.is_file():
+            MI_Files.append(myfile)
+
         myfile = Path(
             "results/" + bin_hash_split[0] + "-" + bin_hash_split[1] + "-E2-R1/" + f)
         if myfile.is_file():
             files.append(myfile)
+
+        myfile = Path(
+            "results/" + bin_hash_split[0] + "_MI-" + bin_hash_split[1] + "-E2-R1/" + f)
+        if myfile.is_file():
+            MI_Files.append(myfile)
+
         myfile = Path(
             "results/" + bin_hash_split[0] + "-" + bin_hash_split[1] + "-E2-R2/" + f)
         if myfile.is_file():
             files.append(myfile)
+
+        myfile = Path(
+            "results/" + bin_hash_split[0] + "_MI-" + bin_hash_split[1] + "-E2-R2/" + f)
+        if myfile.is_file():
+            MI_Files.append(myfile)
+
         myfile = Path(
             "results/" + bin_hash_split[0] + "-" + bin_hash_split[1] + "-E2-R3/" + f)
         if myfile.is_file():
             files.append(myfile)
+
+        myfile = Path(
+            "results/" + bin_hash_split[0] + "_MI-" + bin_hash_split[1] + "-E2-R3/" + f)
+        if myfile.is_file():
+            MI_Files.append(myfile)
+
         myfile = Path("results/" + bin_hash_split[0] + "-" +
                       bin_hash_split[1] + "-E3-" + bin_hash_split[3] + "/" + f)
         if myfile.is_file():
             files.append(myfile)
+
+        myfile = Path("results/" + bin_hash_split[0] + "_MI-" +
+                      bin_hash_split[1] + "-E2-" + bin_hash_split[3] + "/" + f)
+        if myfile.is_file():
+            MI_Files.append(myfile)
+        
         myfile = Path(
             "results/" + bin_hash_split[0] + "-" + bin_hash_split[1] + "-E3-R1/" + f)
         if myfile.is_file():
             files.append(myfile)
+
+        myfile = Path(
+            "results/" + bin_hash_split[0] + "_MI-" + bin_hash_split[1] + "-E3-R1/" + f)
+        if myfile.is_file():
+            MI_Files.append(myfile)
+
         myfile = Path(
             "results/" + bin_hash_split[0] + "-" + bin_hash_split[1] + "-E3-R2/" + f)
         if myfile.is_file():
             files.append(myfile)
+
+        myfile = Path(
+            "results/" + bin_hash_split[0] + "_MI-" + bin_hash_split[1] + "-E3-R2/" + f)
+        if myfile.is_file():
+            MI_Files.append(myfile)
+        
         myfile = Path(
             "results/" + bin_hash_split[0] + "-" + bin_hash_split[1] + "-E3-R3/" + f)
         if myfile.is_file():
             files.append(myfile)
+
+        myfile = Path(
+            "results/" + bin_hash_split[0] + "_MI-" + bin_hash_split[1] + "-E3-R3/" + f)
+        if myfile.is_file():
+            MI_Files.append(myfile)
         try:
             if not files:
                 continue
@@ -113,7 +217,16 @@ with file:
                 test_type = [s for s in name.split('-') if s[1:].isdigit()]
                 engine = test_type[0]
                 reduction = test_type[1]
-                queries.append({"Name": name, "Reduction": reduction, "Engine": engine, "Data": jd["answers"]["Q1"]})
+                queries.append({"Name": name, "Reduction": reduction, "Engine": engine, "Data": jd["answers"]["Q1"], "MI": 0})
+
+            for i in range(len(MI_Files)):
+                jd_mi = json.load(open(MI_Files[i]))
+                name = MI_Files[i].parts[1]
+                test_type = [s for s in name.split('-') if s[1:].isdigit()]
+                engine = test_type[0]
+                reduction = test_type[1]
+                queries.append({"Name": name, "Reduction": reduction, "Engine": engine, "Data": jd_mi["answers"]["Q1"], "MI": 1})
+
 
             # Get universal network stats
             test_numbers = [int(s[1:])
@@ -125,9 +238,10 @@ with file:
             for q in queries:
                 engine = q['Engine']
                 reduction = q['Reduction']
+                mi = q['MI']
                 q = q['Data']
                 verification_compilation.append(
-                    {"Engine": engine, "Reduction": reduction, "Total": q['verification-time'] + q['compilation-time'] + q['reduction-time'],
+                    {"MI": mi, "Engine": engine, "Reduction": reduction, "Total": q['verification-time'] + q['compilation-time'] + q['reduction-time'],
                         "Compilation-time": q["compilation-time"], "Verification-time": q['verification-time'], "Reduction-time": q["reduction-time"], "Output": q['result']})
                 
                 if len([s for s in queries if s['Engine'] == engine]) == 4:
@@ -155,62 +269,45 @@ with file:
             Percentage = 0.5
             Second = 0.001
             # Find fastest engine for verification + reduction
-
-
             i = -1
             for r in ["R0", "R1", "R2", "R3"]:
                 i = i + 1
-                list_post_moped = [s for s in sorted_verification_data if s['Reduction'] == r and s['Engine'] != 'E3']
-                if len(list_post_moped) == 2:
-                    #Even
-                    if (list_post_moped[1]['Verification-time'] - list_post_moped[0]['Verification-time']) / list_post_moped[1]['Verification-time'] < Percentage or list_post_moped[1]['Verification-time'] - list_post_moped[0]['Verification-time'] < Second:
-                        moped_post[2][i] = moped_post[2][i] + 1
-                    elif list_post_moped[0]['Engine'] == 'E1':
-                        moped_post[0][i] = moped_post[0][i] + 1
-                    elif list_post_moped[0]['Engine'] == 'E2':
-                        moped_post[1][i] = moped_post[1][i] + 1
-                else:
-                    moped_post[3][i] = moped_post[3][i] + 1
+                mipost_post = find_win_MI("E2", "E2", i, r, mipost_post, Percentage, Second, sorted_verification_data)
+                moped_post = find_win("E1", "E2", i, r, moped_post, Percentage, Second, sorted_verification_data)
+                moped_pre = find_win("E1", "E3", i, r, moped_pre, Percentage, Second, sorted_verification_data)
+                post_pre = find_win("E1", "E3", i, r, post_pre, Percentage, Second, sorted_verification_data)
 
-                list_pre_moped = [s for s in sorted_verification_data if s['Reduction'] == r and s['Engine'] != 'E2']
-                if len(list_pre_moped) == 2: 
-                    #Even
-                    if (list_pre_moped[1]['Verification-time'] - list_pre_moped[0]['Verification-time']) / list_pre_moped[1]['Verification-time'] < Percentage or list_pre_moped[1]['Verification-time'] - list_pre_moped[0]['Verification-time'] < Second:
-                        moped_pre[2][i] = moped_pre[2][i] + 1
-                    elif list_pre_moped[0]['Engine'] == 'E1':
-                        moped_pre[0][i] = moped_pre[0][i] + 1
-                    elif list_pre_moped[0]['Engine'] == 'E3':
-                        moped_pre[1][i] = moped_pre[1][i] + 1
-                else:
-                    moped_pre[3][i] = moped_pre[3][i] + 1
-
-                list_post_pre = [s for s in sorted_verification_data if s['Reduction'] == r and s['Engine'] != 'E1']
-                if len(list_post_pre) == 2: 
-                    #Even
-                    if (list_post_pre[1]['Verification-time'] - list_post_pre[0]['Verification-time']) / list_post_pre[1]['Verification-time'] < Percentage or list_post_pre[1]['Verification-time'] - list_post_pre[0]['Verification-time'] < Second:
-                        post_pre[2][i] = post_pre[2][i] + 1
-                    elif list_post_pre[0]['Engine'] == 'E2':
-                        post_pre[0][i] = post_pre[0][i] + 1
-                    elif list_post_pre[0]['Engine'] == 'E3':
-                        post_pre[1][i] = post_pre[1][i] + 1
-                else:
-                    post_pre[3][i] = post_pre[3][i] + 1
-
-            test_data.append({"Network": {"Nodes": queries[0]['Data']["network_node_size"], "Labels": queries[0]['Data']["network_label_size"], "Rules": queries[0]['Data']["network_rules_size"],
+            try:
+                test_data.append({"Network": {"Nodes": queries[0]['Data']["network_node_size"], "Labels": queries[0]['Data']["network_label_size"], "Rules": queries[0]['Data']["network_rules_size"],
                                         "States": queries[0]['Data']["pda_states_rules"][0], "PDA_Rules": queries[0]['Data']["pda_states_rules"][1],
                                         "States_reduction": queries[0]['Data']["pda_states_rules_reduction"][0], "Rules_reduction": queries[0]['Data']["pda_states_rules_reduction"][1]},
                             "Query": {"Failover": (int)(Q_fails), "Type": Q_type},
                             "Verification": tuple(sorted_verification_data)})
+            except:
+                print(f)
+                continue
 
         except json.decoder.JSONDecodeError as e:
             print(f)
             print(e)
 
 
-sorted_test_data = sorted(test_data, key=lambda k: k['Verification'][0]['Total'])
+#Print fastest overall time
+print("MI_POSTvsPOST")
+print(mipost_post)
 
-for t in sorted_test_data:
-    print(t)
+i = -1
+for E in mipost_post:
+    i = i + 1
+    if i == 0:
+        engine = "MI\_post"
+    elif i == 1:
+        engine = "Post"
+    elif i == 2:
+        engine = "Even"
+    elif i == 3:
+        engine = "Unknown"
+    print(engine + "&" + str(E[0]) + "&" + str(E[1]) + "&" + str(E[2]) + "&" + str(E[3]) + "\\\ \hline")
 
 
 #Print fastest overall time
@@ -260,10 +357,6 @@ for E in post_pre:
         engine = "Unknown"
     print(engine + "&" + str(E[0]) + "&" + str(E[1]) + "&" + str(E[2]) + "&" + str(E[3]) + "\\\ \hline")
 
-print("Reduction0 win:" + str(red0_win))
-print("Reduction1 win:" + str(red1_win))
-print("Reduction2 win:" + str(red2_win))
-print("Reduction3 win:" + str(red3_win))
 
 #Latex print fastest reduction on fastest engine = 2 :)
 bufferNetworks = []
@@ -294,13 +387,19 @@ for buffer in bufferNetworks:
         print(buffer['Network'] + "&" + str(round(buffer['R0'],3)) + "&" + str(round(buffer['R1'],3)) + "&" + str(round(buffer['R2'],3)) + "&" + str(round(buffer['R3'],3))+ "&" + str(round(speedup,3)) + "\\\ \hline")
 
 #Latex print fastest engine on fastest reducktion = 0
+def printline(list, bi, ei):
+        sorted_engine_ratio = sorted(list, key=lambda k: k['Ratio'])
+        for t in sorted_engine_ratio[bi:ei]:
+            print(str(t['Test']['Network']['Nodes']) + "&" +  str(t['Test']['Network']['Rules']) + "&" + str(t['Test']['Network']['Labels']) 
+                + "&" + str(t['Test']['Network']['States_reduction']) + "&" + str(t['Test']['Network']['Rules_reduction']) + "&" + str(t['Test']['Query']) + "&" + str(t['R'])
+                + "&" + str(round(t['FT'], 4)) + "&" + str(round(t['NT'], 4)) + "&" + str(round(t['Ratio'], 4)) + "\\\ \hline")
+        print("$\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$\\\ \hline")
+
 def get_ratio(engineA, engineB):
     engine_ratio = []
     engine_ratio_false = []
 
     for t in test_data:
-        i = -1
-        
         for r in ["R0", "R1", "R2", "R3"]:
             try:
                 first = next(s for s in t['Verification'] if s['Engine'] == engineA and s['Reduction'] == r)
@@ -321,14 +420,6 @@ def get_ratio(engineA, engineB):
             except:
                 print("some")
                     
-    def printline(list, bi, ei):
-        sorted_engine_ratio = sorted(list, key=lambda k: k['Ratio'])
-        for t in sorted_engine_ratio[bi:ei]:
-            print(str(t['Test']['Network']['Nodes']) + "&" +  str(t['Test']['Network']['Rules']) + "&" + str(t['Test']['Network']['Labels']) 
-                + "&" + str(t['Test']['Network']['States_reduction']) + "&" + str(t['Test']['Network']['Rules_reduction']) + "&" + str(t['Test']['Query']) + "&" + str(t['R'])
-                + "&" + str(round(t['FT'], 4)) + "&" + str(round(t['NT'], 4)) + "&" + str(round(t['Ratio'], 4)) + "\\\ \hline")
-        print("$\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$ & $\\vdots$\\\ \hline")
-
 
     print("True")
 
@@ -339,7 +430,7 @@ def get_ratio(engineA, engineB):
     mid = int(len(engine_ratio)/2)
     printline(engine_ratio, mid, mid + 5)
     last = int(len(engine_ratio))
-    printline(engine_ratio, last - 10, last)
+    printline(engine_ratio, last - 5, last)
 
     print("False")
 
@@ -348,9 +439,55 @@ def get_ratio(engineA, engineB):
     mid = int(len(engine_ratio_false)/2)
     printline(engine_ratio_false, mid, mid + 5)
     last = int(len(engine_ratio_false))
-    printline(engine_ratio_false, last - 10, last)
+    printline(engine_ratio_false, last - 5, last)
 
+def get_ratio_MI(engineA, engineB):
+    engine_ratio = []
+    engine_ratio_false = []
 
+    for t in test_data:
+        for r in ["R0", "R1", "R2", "R3"]:
+            try:
+                first = next(s for s in t['Verification'] if s['Engine'] == engineA and s['Reduction'] == r and s['MI'] == 1)
+                fastest_time = first['Verification-time']
+                next_veri = next(s for s in t['Verification'] if s['Engine'] == engineB and s['Reduction'] == r and s['MI'] == 0)
+                #Remove very fast cases on both engines
+                if  next_veri['Verification-time'] < 0.5 and fastest_time < 0.5: continue
+                if  abs(next_veri['Verification-time'] - fastest_time) < 0.001: continue
+            except:
+                continue
+            try:
+                ratio =  fastest_time / next_veri['Verification-time']
+                #Get and print Output result
+                if first['Output'] == True:
+                    engine_ratio.append({"Test": t, "FT": fastest_time, "R": r, "NT": next_veri['Verification-time'], "NE": next_veri['Engine'], "Ratio": ratio})
+                else:
+                    engine_ratio_false.append({"Test": t, "FT": fastest_time, "R": r, "NT": next_veri['Verification-time'], "NE": next_veri['Engine'], "Ratio": ratio})
+            except:
+                print("some")
+                    
+
+    print("TrueMI")
+
+    print(engineA + engineB)
+    print("\\begin{table}[H]\n\\centering\n\\begin{adjustwidth}{-2cm}{}\n\\begin{tabular}{|c|c|c|c|c|c|c|c|c|c|}\n \hline \n \\rowcolor[HTML]{9B9B9B}\n \\textbf{Nodes}& \\textbf{Rules} & \\textbf{Labels} & \\textbf{PDA-S} & \\textbf{PDA-R} & \\textbf{Query} & \\textbf{R} & \\textbf{V1} & \\textbf{V2} & \\textbf{Ratio}\\\ \hline")
+
+    printline(engine_ratio, 0, 5)
+    mid = int(len(engine_ratio)/2)
+    printline(engine_ratio, mid, mid + 5)
+    last = int(len(engine_ratio))
+    printline(engine_ratio, last - 5, last)
+
+    print("FalseMI")
+
+    print("\\begin{table}[H]\n\\centering\n\\begin{adjustwidth}{-2cm}{}\n\\begin{tabular}{|c|c|c|c|c|c|c|c|c|c|}\n \hline \n \\rowcolor[HTML]{9B9B9B}\n \\textbf{Nodes}& \\textbf{Rules} & \\textbf{Labels} & \\textbf{PDA-S} & \\textbf{PDA-R} & \\textbf{Query} & \\textbf{R} & \\textbf{V1} & \\textbf{V2} & \\textbf{Ratio}\\\ \hline")
+    printline(engine_ratio_false, 0, 5)
+    mid = int(len(engine_ratio_false)/2)
+    printline(engine_ratio_false, mid, mid + 5)
+    last = int(len(engine_ratio_false))
+    printline(engine_ratio_false, last - 5, last)
+
+get_ratio_MI("E2", "E2")
 get_ratio("E1","E2")
 get_ratio("E1","E3")
 get_ratio("E2","E3")
